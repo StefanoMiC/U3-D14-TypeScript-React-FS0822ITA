837e7bae522cdb7fa57fc89eafaacc91214ca316
Stefano Miceli <mic@nucleode.com>
2023-02-23 13:44:50 +0100

    fixes form layout

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,4 +1,4 @@
-import { Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 
@@ -41,52 +41,58 @@ const FormComponent = () => {
   }, []);
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter name"
-          value={firstName}
-          onChange={handleFirstNameChange}
-          required
-        />
-        {/* <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text> */}
-      </Form.Group>
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={8} className="text-start">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+                required
+              />
+              {/* <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text> */}
+            </Form.Group>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Surname</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter last name"
-          value={lastName}
-          onChange={e => {
-            setLastName(e.target.value);
-          }}
-          required
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Check
-          type="checkbox"
-          label="Check me out"
-          checked={checkboxValue}
-          onChange={e => {
-            setCheckboxValue(e.target.checked);
-          }}
-          required
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Click Me
-      </Button>
+            <Form.Group className="mb-3">
+              <Form.Label>Surname</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter last name"
+                value={lastName}
+                onChange={e => {
+                  setLastName(e.target.value);
+                }}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check
+                type="checkbox"
+                label="Check me out"
+                checked={checkboxValue}
+                onChange={e => {
+                  setCheckboxValue(e.target.checked);
+                }}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="d-block mx-auto">
+              Click Me
+            </Button>
 
-      <Link to="/">
-        <Button variant="link" type="submit" className="d-block mx-auto">
-          Then Me to go Home
-        </Button>
-      </Link>
-    </Form>
+            <Link to="/">
+              <Button variant="link" type="submit" className="d-block mx-auto">
+                Then Me to go Home
+              </Button>
+            </Link>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default FormComponent;
